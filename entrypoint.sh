01b725f18ee1220389734a393fb294c8e912daa8
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-09-14 19:08:38 -0300

    refactor(Removing): Removing all the code that is not related to gitops

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# applicate the migrations
-alembic upgrade head
-
-# run server
-uvicorn --host 0.0.0.0 --port 8000 app.main:app
--- tests/test_fixtures.py
+++
@@ -1,4 +0,0 @@
-# import pytest
-# @pytest.mark.anyio
-# async def test_user_skill_assoc_fixture(user_multiples_skills):
-#     pass
