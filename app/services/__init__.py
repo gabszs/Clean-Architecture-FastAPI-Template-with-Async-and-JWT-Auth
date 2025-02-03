ab773476b00aa6afc502ce076caa482e2e014b1b
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2025-02-03 02:07:20 -0300

    feat(ROle): adding the role schema migrations

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,5 +1,6 @@
 from .auth_service import AuthService
+from .role_service import RoleService
 from .tenant_service import TenantService
 from .user_service import UserService
 
-__all__ = ["AuthService", "TenantService", "UserService"]
+__all__ = ["AuthService", "TenantService", "UserService", "RoleService"]
