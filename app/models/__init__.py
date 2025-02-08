5cef34901cd32c924a5384a3a9d3c09fc0efafed
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2025-02-08 04:53:15 -0300

    feat(ApiModels): Adding the new user_tenant and user_role associations to the project

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,9 @@
 from .api_models import Role
 from .api_models import Tenant
 from .api_models import User
+from .api_models import UserRole
+from .api_models import UserTenant
 from .base_model import Base
 
 
-__all__ = ["User", "Skill", "Base", "UserSkillsAssociation", "Tenant", "Role"]
+__all__ = ["User", "Skill", "Base", "UserSkillsAssociation", "Tenant", "Role", "UserRole", "UserTenant"]
--- app/models/api_models.py
+++ app/models/api_models.py
@@ -1,13 +1,14 @@
-from typing import Optional, List
+from typing import List
+from typing import Optional
 from uuid import UUID
 
 from sqlalchemy import ForeignKey
 from sqlalchemy import UniqueConstraint
-from sqlalchemy.orm import Mapped, relationship
+from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
 
 from app.models.base_model import Base
-from app.models.models_enums import UserRoles
 
 
 class Tenant(Base):
@@ -16,25 +17,55 @@ class Tenant(Base):
     name: Mapped[str] = mapped_column(unique=True, nullable=False)
     slug: Mapped[str] = mapped_column(unique=True, nullable=False)
 
-    roles: Mapped[List["Role"]] = relationship(back_populates="tenant", cascade="all, delete-orphan", init=False)
+    users: Mapped[List["UserTenant"]] = relationship(back_populates="tenant", init=False)
+    roles: Mapped[List["Role"]] = relationship(back_populates="tenant", cascade="all, save-update", init=False)
+
+
+class UserTenant(Base):
+    __tablename__ = "user_tenants"
+    # __table_args__ = (UniqueConstraint("user_id", "tenant_id", name="uq_user_role"),)
+
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("users.id"))
+    tenant_id: Mapped[UUID] = mapped_column(ForeignKey("tenants.id"))
+    description: Mapped[Optional[str]]
+
+    user: Mapped["User"] = relationship(back_populates="tenants", init=False)
+    tenant: Mapped[Tenant] = relationship(back_populates="users", init=False)
 
 
 class User(Base):
     __tablename__ = "users"
+    eagers = ["roles", "tenants"]
 
     password: Mapped[str] = mapped_column(nullable=False)
     email: Mapped[str] = mapped_column(unique=True, nullable=False)
     username: Mapped[str] = mapped_column(unique=True)
-    role: Mapped[UserRoles] = mapped_column(default=UserRoles.BASE_USER, server_default=UserRoles.BASE_USER)
+
+    roles: Mapped[List["UserRole"]] = relationship(back_populates="user", init=False)
+    tenants: Mapped[List["UserTenant"]] = relationship(back_populates="user", init=False)
+
     is_active: Mapped[bool] = mapped_column(default=True, server_default="True")
 
 
 class Role(Base):
     __tablename__ = "roles"
+    __table_args__ = (UniqueConstraint("tenant_id", "name", name="uq_tenant_name"),)
 
-    tenant_id: Mapped[UUID] = mapped_column(ForeignKey("tenants.id"))
     name: Mapped[str] = mapped_column(nullable=False)
     description: Mapped[Optional[str]]
+    tenant_id: Mapped[UUID] = mapped_column(ForeignKey("tenants.id", ondelete="SET NULL"))
     tenant: Mapped[Tenant] = relationship(back_populates="roles", init=False)
 
-    __table_args__ = (UniqueConstraint("tenant_id", "name", name="uq_tenant_name"),)
+    users: Mapped[List["UserRole"]] = relationship(back_populates="role", init=False)
+
+
+class UserRole(Base):
+    __tablename__ = "user_roles"
+    __table_args__ = (UniqueConstraint("user_id", "role_id", name="uq_user_role"),)
+
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("users.id"))
+    role_id: Mapped[UUID] = mapped_column(ForeignKey("roles.id"))
+    description: Mapped[Optional[str]]
+
+    user: Mapped["User"] = relationship(back_populates="roles", init=False)
+    role: Mapped["Role"] = relationship(back_populates="users", init=False)
