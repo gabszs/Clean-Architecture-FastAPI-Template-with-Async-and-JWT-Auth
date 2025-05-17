d2c50e26d38dbfe522352ade0cd961cf02729b32
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2025-05-17 02:14:20 -0300

    feat(logger): defining logging configuration at app lifespan
    also adding init and shutdown logs

--- app/main.py
+++ app/main.py
@@ -1,22 +1,31 @@
+import logging
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
 from app.core.database import sessionmanager
 from app.core.settings import settings
-from app.routes.v1 import routers
+from app.routes import app_routes
 
 
 def init_app(init_db=True):
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    logging.getLogger("opentelemetry").propagate = False
+    # logger.addHandler(logging.StreamHandler()) # use this to print the logs in the console
+
     lifespan = None
 
     if init_db:
+        print(f"DATABASE_URL{settings.DATABASE_URL:}")
         sessionmanager.init(settings.DATABASE_URL)
         # sessionmanager.run_migrations()
 
         @asynccontextmanager
         async def lifespan(app: FastAPI):
+            logger.info(f"{settings.PROJECT_NAME} initialization started.")
             yield
+            logger.info(f"{settings.PROJECT_NAME} shutdown completed.")
             if sessionmanager._engine is not None:
                 await sessionmanager.close()
 
@@ -31,7 +40,7 @@ def init_app(init_db=True):
         summary="WebAPI built on best market practices such as TDD, Clean Architecture, Data Validation with Pydantic V2",
         lifespan=lifespan,
     )
-    app.include_router(routers)
+    app.include_router(app_routes)
 
     return app
 
