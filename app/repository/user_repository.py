72b9acf57b851bb002a3565b16fefb322f1b01e3
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2025-05-17 02:11:00 -0300

    refactor(repository): remocing old style session with context, remaning the erros and adding logs
    also addin otel instrumentalization

--- app/repository/user_repository.py
+++ app/repository/user_repository.py
@@ -1,30 +1,31 @@
-from contextlib import AbstractAsyncContextManager
-from typing import Callable
-
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.exceptions import DuplicatedError
+from app.core.telemetry import instrument
 from app.models import User
 from app.repository.base_repository import BaseRepository
 
 
+@instrument
 class UserRepository(BaseRepository):
-    def __init__(self, session_factory: Callable[..., AbstractAsyncContextManager[Session]]):
-        self.session_factory = session_factory
-        super().__init__(session_factory, User)
+    def __init__(
+        self,
+        session: AsyncSession,
+    ):
+        self.session = session
+        super().__init__(session, User)
 
     async def create(self, schema):
-        async with self.session_factory() as session:
-            model = self.model(**schema.model_dump())
-            try:
-                session.add(model)
-                await session.commit()
-                await session.refresh(model)
-            except IntegrityError as e:
-                if "Key (email)" in str(e.orig):
-                    raise DuplicatedError(detail="Email already registered")
-                if "Key (username)" in str(e.orig):
-                    raise DuplicatedError(detail="Username already registered")
-                raise DuplicatedError(detail=f"{self.model.__tablename__.capitalize()[:-1]} already registered")
-            return model
+        model = self.model(**schema.model_dump())
+        try:
+            self.session.add(model)
+            await self.session.commit()
+            await self.session.refresh(model)
+        except IntegrityError as e:
+            if "Key (email)" in str(e.orig):
+                raise DuplicatedError(detail="Email already registered")
+            if "Key (username)" in str(e.orig):
+                raise DuplicatedError(detail="Username already registered")
+            raise DuplicatedError(detail=f"{self.model.__tablename__.capitalize()[:-1]} already registered")
+        return model
